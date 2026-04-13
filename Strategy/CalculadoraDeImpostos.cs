5b09dd3dc6f8b6abdbc62b84e02b189c9017707e
FelipeLaus <felipe.laus10@gmail.com>
2026-04-13 19:51:17 -0300

    Cleaning up the coding

--- Strategy/CalculadoraDeImpostos.cs
+++ Strategy/CalculadoraDeImpostos.cs
@@ -1,7 +1,4 @@
 ﻿using Strategy.Impostos;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Strategy
 {
--- Strategy/Impostos/Icms.cs
+++ Strategy/Impostos/Icms.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Strategy.Impostos
+﻿namespace Strategy.Impostos
 {
     public class Icms : Imposto
     {
